52f125792d5f798ee7c1a0b9ce4d22f64c7dd27f
robbinhan <luckyhanbin@gmail.com>
2017-09-16 00:15:56 +0800

    update

--- src/main/java/com/corundumstudio/socketio/store/RedissonStore.java
+++ src/main/java/com/corundumstudio/socketio/store/RedissonStore.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 Nikita Koksharov
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,17 +15,20 @@
  */
 package com.corundumstudio.socketio.store;
 
+import org.redisson.api.RList;
+import org.redisson.api.RedissonClient;
+
 import java.util.Map;
 import java.util.UUID;
-
-import org.redisson.api.RedissonClient;
 
 public class RedissonStore implements Store {
 
     private final Map<String, Object> map;
+    private final RedissonClient redisson;
 
     public RedissonStore(UUID sessionId, RedissonClient redisson) {
         this.map = redisson.getMap(sessionId.toString());
+        this.redisson = redisson;
     }
 
     @Override
@@ -48,4 +51,7 @@ public class RedissonStore implements Store {
         map.remove(key);
     }
 
+    public RedissonClient getRedisson() {
+        return redisson;
+    }
 }
